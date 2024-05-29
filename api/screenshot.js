59ce179a535dedcfff94045168e1e53622935e60
breezyairdropp <breezyairdrop@gmail.com>
2024-05-30 00:56:51 +0100

    Update screenshot.js

--- api/screenshot.js
+++ api/screenshot.js
@@ -9,7 +9,6 @@
 // or 900 seconds (Enterprise plan).
 
 const puppeteer = require("puppeteer-core");
-const chrome = require("chrome-aws-lambda");
 
 /** The code below determines the executable location for Chrome to
  * start up and take the screenshot when running a local development environment.
@@ -21,30 +20,6 @@ const chrome = require("chrome-aws-lambda");
  * your Chrome installation on your operating system.
  */
 
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
-
-async function getOptions(isDev) {
-  let options;
-  if (isDev) {
-    options = {
-      args: [],
-      executablePath: exePath,
-      headless: true,
-    };
-  } else {
-    options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    };
-  }
-  return options;
-}
 
 module.exports = async (req, res) => {
   const pageToScreenshot = req.query.page;
@@ -52,7 +27,7 @@ module.exports = async (req, res) => {
   // pass in this parameter if you are developing locally
   // to ensure puppeteer picks up your machine installation of
   // Chrome via the configurable options
-  const isDev = req.query.isDev === "true";
+  // const isDev = req.query.isDev === "true";
 
   try {
     // check for https for safety!
@@ -64,10 +39,13 @@ module.exports = async (req, res) => {
     }
 
     // get options for browser
-    const options = await getOptions(isDev);
+    // const options = await getOptions(isDev);
 
     // launch a new headless browser with dev / prod options
-    const browser = await puppeteer.launch(options);
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      executablePath: puppeteer.executablePath()
+    });
     const page = await browser.newPage();
 
     // set the viewport size
